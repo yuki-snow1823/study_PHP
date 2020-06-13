5bfb4575a4093ad9d85b59ec40e7ee7237729839
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-13 16:27:28 +0900

    [add]配列のkey設定

--- main.php
+++ main.php
@@ -117,3 +117,14 @@ $scores = [
 
 $scores[1] = 60;
 echo $scores[1] . PHP_EOL;
+
+$scores = [
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
+];
+
+// var_dump($scores);
+// print_r($scores);
+
+echo $scores['third'] . PHP_EOL;
