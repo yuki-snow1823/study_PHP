051360a1955a1bc1cf44a3d1543a275b575484f3
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-13 16:29:01 +0900

    [add]foreach、配列の値に一つ一つ処理

--- main.php
+++ main.php
@@ -128,3 +128,18 @@ $scores = [
 // print_r($scores);
 
 echo $scores['third'] . PHP_EOL;
+
+$scores = [
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
+];
+
+// foreach ($scores as $value) {
+// foreach ($scores as $score) {
+//   echo $score . PHP_EOL;
+// }
+
+foreach ($scores as $key => $score) {
+  echo $key . ' - ' . $score . PHP_EOL;
+}
