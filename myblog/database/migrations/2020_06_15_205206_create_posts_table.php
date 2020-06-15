caf7d8cc51650905ff47d08cf0be1ddf7ca2e278
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-15 20:56:00 +0900

    [add]マイグレーション

---
+++ myblog/database/migrations/2020_06_15_205206_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    // やりたい処理
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    // 戻したい処理
+    {
+        Schema::dropIfExists('posts');
+    }
+}
