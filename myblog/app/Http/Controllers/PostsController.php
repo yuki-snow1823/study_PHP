14aa0bbb9d8990b855af434e51070dbc5519d9e8
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-16 23:07:47 +0900

    [add]データの抽出とデバッグ

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     //
     public function index() {
-        // 名前の指定
+    // 名前の指定
+    //   $posts = Post::all();
+    //   $posts = Post::orderBy('created_at', 'desc')->get();
+      $posts = Post::latest()->get();
+    //   dd($posts->toArray());
       return view('posts.index');
     }
 }
