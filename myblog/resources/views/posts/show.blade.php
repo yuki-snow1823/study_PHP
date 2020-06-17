66b9d292e1b0761ca9bb8900d447c920f95f0928
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-17 10:16:02 +0900

    [add]詳細画面の作成

---
+++ myblog/resources/views/posts/show.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>{{ $post->title }}</title>
+  <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+  <div class="container">
+    <h1>{{ $post->title }}</h1>
+    <!-- {{!!}}中身をエスケープしない -->
+    <p>{!! nl2br(e($post->body)) !!}</p>
+  </div>
+</body>
+</html>
