67be8487cd7da3d1aaee324d4fc90c6f948571a8
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-19 16:08:09 +0900

    [add]受け取る値を指定したルーティングの設定

--- myblog/routes/web.php
+++ myblog/routes/web.php
@@ -19,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'PostsController@index');
 // Route::get('/posts/{id}', 'PostsController@show');
-Route::get('/posts/{post}', 'PostsController@show');
+// Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/{post}', 'PostsController@show')->where('post', '[0-9]+');
+Route::get('/posts/create', 'PostsController@create');
