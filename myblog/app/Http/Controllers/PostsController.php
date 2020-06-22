161bdf15db608fac0338650f11ab152956f5a02a
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-23 08:05:45 +0900

    [add]requestの読み込み、不要なものの削除

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Post;
+use App\Http\Requests\PostRequest;
 
 class PostsController extends Controller
 {
@@ -31,11 +31,7 @@ class PostsController extends Controller
       return view('posts.create');
     }
 
-    public function store(Request $request) {
-      $this->validate($request, [
-        'title' => 'required|min:3',
-        'body' => 'required'
-      ]);
+    public function store(PostRequest $request) {
       $post = new Post();
       $post->title = $request->title;
       $post->body = $request->body;
@@ -46,12 +42,8 @@ class PostsController extends Controller
     public function edit(Post $post) {
       return view('posts.edit')->with('post', $post);
     }
-    
-    public function update(Request $request, Post $post) {
-      $this->validate($request, [
-        'title' => 'required|min:3',
-        'body' => 'required'
-      ]);
+
+    public function update(PostRequest $request, Post $post) {
       $post->title = $request->title;
       $post->body = $request->body;
       $post->save();
