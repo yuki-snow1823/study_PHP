3194e2190d9f7bcd3c3fce6d05630233c9925da2
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-23 16:05:11 +0900

    [add]コメント機能の追加

--- myblog/app/Comment.php
+++ myblog/app/Comment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+    Protected $fillable = ["body"];
+
+    public function post () {
+        return $this->belongsTo('App\Post');
+    }
 }
--- myblog/resources/views/posts/show.blade.php
+++ myblog/resources/views/posts/show.blade.php
@@ -1,19 +1,22 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>{{ $post->title }}</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <div class="container">
-    
-    <h1>
-      <a href="{{ url("/") }}" class="header-menu">Back</a>
-      {{ $post->title }}
-    </h1>
-    <p>{!! nl2br(e($post->body)) !!}</p>
-    
-  </div>
-</body>
-</html>
+@extends('layouts.default')
+
+@section('title', $post->title)
+
+@section('content')
+<h1>
+  <a href="{{ url('/') }}" class="header-menu">Back</a>
+  {{ $post->title }}
+</h1>
+<p>{!! nl2br(e($post->body)) !!}</p>
+
+<h2>Comments</h2>
+<ul>
+  @forelse ($post->comments as $comment)
+  <li>
+    {{ $comment->body }}
+  </li>
+  @empty
+  <li>No comments yet</li>
+  @endforelse
+</ul>
+@endsection
