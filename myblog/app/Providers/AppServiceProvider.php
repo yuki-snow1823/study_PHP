2f48c0cdc2cc5317aada80f1880d12d039c55e00
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-23 16:34:02 +0900

    [add]外部キー制約の設定

--- myblog/app/Providers/AppServiceProvider.php
+++ myblog/app/Providers/AppServiceProvider.php
@@ -7,21 +7,23 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+      if (\DB::getDriverName() === 'sqlite') {
+        \DB::statement(\DB::raw('PRAGMA foreign_keys=1'));
+      }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
