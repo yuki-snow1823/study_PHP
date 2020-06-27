daac23c23b70661a0c76ebfe13faf32c06f9451e
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-28 01:20:02 +0900

    [add]挿入ソート

--- sort_study/insertion_sort.php
+++ sort_study/insertion_sort.php
@@ -1,19 +1,25 @@
 <?php
-$numbers = array(1,3,5,4,2);
+$numbers = array(1,3,5,4,2,6);
 // 挿入ソート
 
 // 出力
-var_dump(bubble_sort($numbers));
+var_dump(insertion_sort($numbers));
 
 function insertion_sort($array)
 {
-    for($i = 1; $i < count($array)-1; $i++)
+    for($i = 1; $i < count($array); $i++)
     {
       $new_number = $array[$i];
       $old_number_position = $i - 1;
 
       while($old_number_position > 0){
-        
+        if($new_number < $array[$old_number_position]) {
+          $temp_number = $array[$old_number_position];
+          $array[$old_number_position] = $array[$old_number_position + 1];
+          $array[$old_number_position + 1] = $temp_number;
+        }
+        $old_number_position -= 1;
+        print_r($array);
       }
     }
     return $array;
