f6368bfd9a9d916ce2d02c8e702060567cb982e2
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-29 20:40:13 +0900

    [add]heapsortの学習

--- sort_study/insertion_sort.php
+++ sort_study/insertion_sort.php
@@ -18,7 +18,8 @@ function insertion_sort($array)
       while($old_number_position > 0){
         // 比較する数字が、される数字より小さかった場合、入れ替える
         if($new_number < $array[$old_number_position]) {
-          // 多重代入の方法がわからなかったため、一時保管するための$temp_numberを使用
+          // 多重代入の方法がよくわからなかったため、一時保管するための$temp_numberを使用
+          // https://scrapbox.io/php/%E5%A4%9A%E9%87%8D%E4%BB%A3%E5%85%A5 これでできるらしいですが…。
           $temp_number = $array[$old_number_position];
           $array[$old_number_position] = $array[$old_number_position + 1];
           $array[$old_number_position + 1] = $temp_number;
