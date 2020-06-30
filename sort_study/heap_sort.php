a81046b29c8d81b876677a9a843bfcc5f466abda
yuki-snow1823 <magic.iroha@gmail.com>
2020-06-30 11:07:25 +0900

    [add]学習

--- sort_study/heap_sort.php
+++ sort_study/heap_sort.php
@@ -1,51 +1,51 @@
 <?php
-// ヒープの作成
+// ヒープの作成？
 // 元の値が書き変わる参照渡しを使用(&$array)
- function make_heap(&$array, $i, $t){
-   // まずは適当に入れたものを並び替える
-  $tmp_var = $array[$i];
-  // ? iは多分親のノードの番号jは子、いや、iが子かも
-  // イメージ：三角形を見て、比較する感じか
-  // まず、親と子を比較、で、もし下が小さければ、次に子を見て左右の大きい方を親にする
-  // https://www.youtube.com/watch?v=X0ESspSiLIc 2:31
-  // で、数列に入れたら、ヒープの修正をする感じ
-  $j = $i * 2 + 1;
+ function make_heap(&$array, $parent_node_index, $t){
+   // 親のノードの数字と子のインデックスを定義
+  $parent_number = $array[$parent_node_index];
+  $left_children_node_index = $parent_node_index * 2 + 1;
 
-  while ($j <= $t)  {
-   if($j < $t)
-   // 理解：二つの親を比較した時、右の親の方が大きな数字だった場合
-    if($array[$j] < $array[$j + 1]) {
-      // 左右を入れ替える？
-     $j = $j + 1; 
+  //
+  while ($left_children_node_index <= $t)  {
+    // 同じではなく大きい場合
+   if($left_children_node_index < $t)
+   // 左右の子を比較した時、右の子の方が大きな数字だった場合
+    if($array[$left_children_node_index] < $array[$left_children_node_index + 1]) {
+      // 左右を入れ替える
+     $left_children_node_index = $left_children_node_index + 1; 
     }
-    // 子の要素の数字より、親要素の数字が大きかった場合(ダメなの？)
-   if($tmp_var < $array[$j]) {
-    $array[$i] = $array[$j];
-    $i = $j;
-    $j = 2 * $i + 1;
+    // 親のノードの数字より、左の子のノードの数字が大きい場合
+   if($parent_number < $array[$left_children_node_index]) {
+     // 親子を数字、インデックス共に入れ替える
+    $array[$parent_node_index] = $array[$left_children_node_index];
+    $parent_node_index = $left_children_node_index;
+    $left_children_node_index = 2 * $parent_node_index + 1;
+    // 
    } else {
-    $j = $t + 1;
+    $left_children_node_index = $t + 1;
    }
   }
-  $array[$i] = $tmp_var;
+  // 配列の親のインデックスの位置に、親の数字を入れる
+  $array[$parent_node_index] = $parent_number;
  }
 
  function heap_sort(&$array) {
-  //This will heapify the array
-  // 整数に変換
-  $init = (int)floor((count($array) - 1) / 2);
-  // $iの初期値は、arrayの-1して/2した整数回数
-  // make_heapを繰り返すことで?
-  for($i=$init; $i >= 0; $i--){
+
+  // 配列をヒープにするアルゴリズム？
+  // 親ノードのインデックスは(配列の総数-1)/2で表すことができる
+  $parent_node_index = (int)floor((count($array) - 1) / 2);
+  // 全ての親の場合を行うため、$parent_node_indexを減らしていく
+  for($parent_node_index; $parent_node_index >= 0; $parent_node_index--){
    $count = count($array) - 1;
    make_heap($array, $i, $count);
   }
 
   // ノードの入れ替え
   for ($i = (count($array) - 1); $i >= 1; $i--)  {
-   $tmp_var = $array[0];
+   $parent_number = $array[0];
    $array [0] = $array [$i];
-   $array [$i] = $tmp_var;
+   $array [$i] = $parent_number;
    make_heap($array, 0, $i - 1);
   }
  }
