6d83cd068386f2fb9100b8eb77d8155a9c5f4a78
yuki-snow1823 <magic.iroha@gmail.com>
2020-07-29 23:33:11 +0900

    add:ファサードの学習

--- chapter03/config/app.php
+++ chapter03/config/app.php
@@ -174,7 +174,7 @@ return [
     */
 
     'aliases' => [
-
+// ファサードと呼ばれるもの
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
