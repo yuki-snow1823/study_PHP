62b6a7dddf971b18b49842216e163ece9972c92b
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-17 23:17:28 +0900

    urlの値を引数としてとり、表示

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -17,7 +17,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
-});
+Route::get('hello/{msg}', function ($msg) {
+    $html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-align:right; color:#eee;
+   margin:-40px 0px -50px 0px; }
+</style>
+</head>
+<body>
+   <h1>Hello</h1>
+   <p>{$msg}</p>
+   <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
 
+    return $html;
+});
