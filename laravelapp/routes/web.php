b2d03ed1e4faf0c10efa3b0efbc63db1c5b0a722
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-18 06:59:27 +0900

    request/response

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -17,24 +17,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{msg}', function ($msg) {
-    $html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:100pt; text-align:right; color:#eee;
-   margin:-40px 0px -50px 0px; }
-</style>
-</head>
-<body>
-   <h1>Hello</h1>
-   <p>{$msg}</p>
-   <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
+Route::get('hello', 'HelloController@index');
 
-    return $html;
-});
