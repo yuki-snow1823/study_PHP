04bd6920cefe9fe525ff1cb5fed7264cb8b9cb6b
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-21 21:38:20 +0900

    サービスコンテナ

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -11,6 +11,19 @@
 @section('content')
    <p>ここが本文のコンテンツです。</p>
    <p>必要なだけ記述できます。</p>
+  
+   @component('components.message')
+   {{-- しっかり覚えるslot --}}
+       @slot('msg_title') 
+       {{-- msg_titleという変数にcautionが入る！ --}}
+       CAUTION!!
+       @endslot
+
+       @slot('msg_content')
+       これはメッセージの表示です。
+       @endslot
+   @endcomponent
+
 @endsection
 
 @section('footer')
