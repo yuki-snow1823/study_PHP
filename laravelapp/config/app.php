d5de73834e2e2f2be8ce9eeab1b87f29a46d9e86
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-22 10:13:50 +0900

    プロバイダーの登録

--- laravelapp/config/app.php
+++ laravelapp/config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // 追加
+        App\Providers\HelloServiceProvider::Class
+
+
     ],
 
     /*
