a26aece0d25570d8f88c549b43f07c94822ed1c8
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-29 11:54:39 +0900

    @errorディレクティブ

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -9,31 +9,35 @@
 @endsection
 
 @section('content')
-   <p>{{$msg}}</p>
-   @if (count($errors) > 0)
-   <p>入力に問題があります。再入力して下さい。</p>
-   @endif
-   <table>
-   <form action="/hello" method="post">
-       @csrf
-       @if ($errors->has('name'))
-       <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
-       @endif
-       <tr><th>name: </th><td><input type="text" name="name"
-           value="{{old('name')}}"></td></tr>
-       @if ($errors->has('mail'))
-       <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
-       @endif
-       <tr><th>mail: </th><td><input type="text" name="mail"
-           value="{{old('mail')}}"></td></tr>
-       @if ($errors->has('age'))
-       <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
-       @endif
-       <tr><th>age: </th><td><input type="text" name="age"
-           value="{{old('age')}}"></td></tr>
-       <tr><th></th><td><input type="submit" value="send"></td></tr>
-   </table>
-   </form>
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+        <p>入力に問題があります。再入力して下さい。</p>
+    @endif
+    <form action="/hello" method="post">
+    <table>
+        @csrf
+        @error('name')
+            <tr><th>ERROR</th>
+            <td>{{$message}}</td></tr>
+        @enderror
+        <tr><th>name: </th><td><input type="text" 
+            name="name" value="{{old('name')}}"></td></tr>
+        @error('mail')
+            <tr><th>ERROR</th>
+            <td>{{$message}}</td></tr>
+        @enderror
+        <tr><th>mail: </th><td><input type="text" 
+            name="mail" value="{{old('mail')}}"></td></tr>
+        @error('age')
+            <tr><th>ERROR</th>
+            <td>{{$message}}</td></tr>
+        @enderror
+        <tr><th>age: </th><td><input type="text" 
+            name="age" value="{{old('age')}}"></td></tr>
+        <tr><th></th><td><input type="submit" value="send">
+        </td></tr>
+    </table>
+    </form>
 @endsection
 
 @section('footer')
