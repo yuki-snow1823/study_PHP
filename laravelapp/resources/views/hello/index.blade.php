5bd7c58d6747cb7ba7469523b11265a65f5f44a4
yuki-snow1823 <magic.iroha@gmail.com>
2020-08-29 11:53:25 +0900

    過去の値の維持とエラーの値ごとの出力

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -10,17 +10,28 @@
 
 @section('content')
    <p>{{$msg}}</p>
-   <form action="/hello" method="post">
+   @if (count($errors) > 0)
+   <p>入力に問題があります。再入力して下さい。</p>
+   @endif
    <table>
+   <form action="/hello" method="post">
        @csrf
-       <tr><th>name: </th><td><input type="text" 
-            name="name"></td></tr>
-       <tr><th>mail: </th><td><input type="text" 
-            name="mail"></td></tr>
-       <tr><th>age: </th><td><input type="text" 
-            name="age"></td></tr>
-       <tr><th></th><td><input type="submit" 
-            value="send"></td></tr>
+       @if ($errors->has('name'))
+       <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+       @endif
+       <tr><th>name: </th><td><input type="text" name="name"
+           value="{{old('name')}}"></td></tr>
+       @if ($errors->has('mail'))
+       <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+       @endif
+       <tr><th>mail: </th><td><input type="text" name="mail"
+           value="{{old('mail')}}"></td></tr>
+       @if ($errors->has('age'))
+       <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+       @endif
+       <tr><th>age: </th><td><input type="text" name="age"
+           value="{{old('age')}}"></td></tr>
+       <tr><th></th><td><input type="submit" value="send"></td></tr>
    </table>
    </form>
 @endsection
